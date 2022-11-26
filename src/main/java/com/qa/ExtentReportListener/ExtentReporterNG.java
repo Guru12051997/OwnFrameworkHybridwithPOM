c61d03dd1b01f042c353418cae9f1b3a8d27d956
GURU <gurupdas11@gmail.com>
2022-11-26 16:11:55 +0530

    Final commit

--- src/main/java/com/qa/ExtentReportListener/ExtentReporterNG.java
+++ src/main/java/com/qa/ExtentReportListener/ExtentReporterNG.java
@@ -18,7 +18,7 @@ import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 
-/*public class ExtentReporterNG implements IReporter {
+public class ExtentReporterNG implements IReporter {
 	private ExtentReports extent;
 
 	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
@@ -70,4 +70,4 @@ import com.relevantcodes.extentreports.LogStatus;
 		return calendar.getTime();
 	}
 
-}*/
+}
