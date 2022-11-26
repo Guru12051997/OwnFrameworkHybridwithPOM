459541425b89f4e80f38b92351fc1c9cf500e4a7
GURU <gurupdas11@gmail.com>
2022-11-26 11:36:41 +0530

    Final commit

---
+++ src/main/java/com/qa/util/Nothing.java
@@ -0,0 +1,14 @@
+package com.qa.util;
+
+public class Nothing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("GuruPrasad Das"); 
+System.out.println("GuruPrasad Das");
+System.out.println("GuruPrasad Das");
+System.out.println("GuruPrasad Das");
+System.out.println("GuruPrasad Das");
+	}
+
+}
